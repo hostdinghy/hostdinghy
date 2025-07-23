a37ac47d372cc864fabb33f3a3e8f4cefb0ad855
Sören Meier <soeren@s-me.ch>
2025-07-23 22:55:10 +0200

    improve rule parsing

--- huus/src/traefik/utils.rs
+++ huus/src/traefik/utils.rs
@@ -3,7 +3,7 @@ use std::sync::LazyLock;
 use regex::Regex;
 
 static DOMAIN_REGEX: LazyLock<Regex> =
-	LazyLock::new(|| Regex::new(r"(?:[^!]|^)Host\(`(.+?)`\)").unwrap());
+	LazyLock::new(|| Regex::new(r"(?:[|&]|^|\s)Host\(`(.+?)`\)").unwrap());
 
 // parse Host(`abc`) || Host(`domain`)&&Host(`ccc`) && !Host(`ignore`)
 pub fn parse_rule_to_domains(rule: &str) -> Vec<String> {

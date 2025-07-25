e126ab865051d5e1623c38411675a6b7116bb979
Thierry Muehlemann <33832725+Thierry-Muehlemann@users.noreply.github.com>
2025-07-25 17:30:38 +0200

    huus-ui status component

---
+++ huus-ui/src/components/Status.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+	type State =
+		| 'empty'
+		| 'created'
+		| 'unhealthy'
+		| 'running'
+		| 'paused'
+		| 'restarting'
+		| 'exited'
+		| 'removing'
+		| 'dead'
+		| 'unknown';
+
+	let { value, size = 'md' }: { value: State; size: 'sm' | 'md' | 'lg' } =
+		$props();
+</script>
+
+<div class="status {size}" data-status={value}></div>
+
+<style>
+	.status {
+		border-radius: 50%;
+		border: 0.1rem solid transparent;
+		flex-shrink: 0;
+	}
+	.sm {
+		width: 0.25rem;
+		height: 0.25rem;
+	}
+	.md {
+		width: 0.5rem;
+		height: 0.5rem;
+	}
+	.lg {
+		width: 0.75rem;
+		height: 0.75rem;
+	}
+
+	[data-status='running'] {
+		background-color: var(--green);
+		border-color: var(--green);
+	}
+
+	[data-status='created'] {
+		background-color: var(--blue);
+		border-color: var(--blue);
+	}
+
+	[data-status='unhealthy'] {
+		background-color: var(--red);
+		border-color: var(--red);
+	}
+
+	[data-status='running'] {
+		background-color: var(--green);
+		border-color: var(--green);
+	}
+
+	[data-status='paused'] {
+		background-color: var(--yellow);
+		border-color: var(--yellow);
+	}
+
+	[data-status='restarting'] {
+		background-color: var(--orange);
+		border-color: var(--orange);
+	}
+
+	[data-status='removing'] {
+		background-color: var(--purple);
+		border-color: var(--purple);
+	}
+
+	[data-status='empty'],
+	[data-status='exited'] {
+		background-color: transparent;
+		&.sm {
+			background-color: var(--c-border);
+		}
+		border-color: var(--c-border);
+	}
+	[data-status='unknown'],
+	[data-status='dead'] {
+		background-color: transparent;
+		&.sm {
+			background-color: var(--purple);
+		}
+		border-color: var(--purple);
+	}
+</style>
--- huus-ui/src/pages/Index.svelte
+++ huus-ui/src/pages/Index.svelte
@@ -1,6 +1,7 @@
 <script module>
 	import { all, App } from '@/api/apps';
 	import Button from '@/components/Button.svelte';
+	import Status from '@/components/Status.svelte';
 
 	export async function loadProps() {
 		return {
@@ -22,11 +23,11 @@
 	<Table
 		headers={[
 			{ key: 'name', value: 'Name' },
-			{ key: 'protocol', value: 'Status' },
+			{ key: 'status', value: 'Status' },
 			{ key: 'port', value: 'Uptime' },
 			{ key: 'rule', value: 'Actions' },
 		]}
-		rows={apps}
+		rows={apps.map(a => ({ ...a, status: 'running' }))}
 	>
 		{#snippet toolbar()}
 			<Button href="/apps/create">add</Button>
@@ -37,11 +38,29 @@
 				<a class="underline" href="/apps/{row.id}">{row.name}</a>
 			</td>
 		{/snippet}
+
+		{#snippet status(row)}
+			<td>
+				<div class="status">
+					<Status value={row.status} />
+					{row.status}
+				</div>
+			</td>
+		{/snippet}
 	</Table>
 </div>
 
 <style>
 	.layout {
 		margin-top: 5rem;
+	}
+	.status {
+		display: flex;
+		gap: 0.5rem;
+		align-items: center;
+	}
+
+	.demo {
+		margin-top: 2rem;
 	}
 </style>
